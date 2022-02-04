6e95a2fac1bb9bac21ab7cbee9d368f04c710723
최승연 <syc1008@naver.com>
2022-02-04 09:48:54 +0900

    feature|club 메인, 클럽 생성, 검색창 UI

---
+++ frontend/src/views/club/components/AlertFailJoinClub.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-dialog
+    v-model="show"
+    width="30rem"
+  >
+    <v-card id="alert-fail-join-club">
+      <v-card-text class="mt-10">
+        <p>정원 초과로 클럽 가입이 불가합니다</p>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn
+          icon
+        >
+          <v-icon>mdi-checkbox-marked-circle</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'AlertFailJoinClub',
+  props: {
+    value: Boolean,
+  },
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+#alert-fail-join-club {
+  padding: 2rem;
+  border-radius: 1rem;
+  text-align: center;
+}
+</style>
---
+++ frontend/src/views/club/components/ClubListElement.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <img :src='`${ UserClub.club_img }`' alt="club-image">
+    <h3 class="my-3">{{ UserClub.club_name }}</h3>
+    <div class="d-flex justify-space-between">
+      <div class="d-flex">
+        <span><v-icon>mdi-alarm</v-icon></span>
+        <p>{{ UserClub.Participation_day.mon }} {{ UserClub.fix_time }}</p>
+      </div>
+      <div class="d-flex">
+        <span><v-icon>mdi-account-circle</v-icon></span>
+        <p>{{ UserClub.count }} / {{ UserClub.count }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClubListElement',
+  data() {
+    return {
+      UserClub: {
+        club_name: '클럽1',
+        club_img: require('@/assets/club/club_default.png'),
+        Participation_day: {
+          mon: true,
+          tues: true,
+          wedn: true,
+          thur: true,
+          fri: true,
+          sau: true,
+          sun: true,
+        },
+        manager: '클럽장이름',
+        start_date: '2019-09-10',
+        end_date: '2019-09-20',
+        fix_time: '15:00',
+        count: 6,
+        finish: false,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.gallery-item > img {
+  height: 200px;
+  width: 200px;
+  border-radius: 1rem;
+}
+.gallery-item span {
+  margin: 0px 5px 0px 0px;
+}
+.gallery-container {
+  display: flex;
+  flex-direction: row;
+  overflow-x: scroll;
+}
+</style>
