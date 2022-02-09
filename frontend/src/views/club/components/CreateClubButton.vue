212064eb0557a381fa51eb0314e07ff2dfe55542
최승연 <syc1008@naver.com>
2022-02-09 16:04:50 +0900

    feature|club 클럽 관리, 클럽트레이닝

---
+++ frontend/src/views/club/components/CreateClubButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog
+    width="45rem"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        small
+        rounded
+        color="amber lighten-5"
+        elevation="1"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <span>클럽 만들기</span>
+      </v-btn>
+    </template>
+    <DialogCreateClub/>
+  </v-dialog>
+</template>
+<script>
+import DialogCreateClub from '@/views/club/components/DialogCreateClub.vue';
+
+export default {
+  name: 'CreateClubButton',
+  components: {
+    DialogCreateClub,
+  },
+};
+</script>
+<style scoped>
+#club-main-btn .v-btn{
+  height: 2rem;
+  width: 9rem;
+  margin: 0rem 0rem 0rem 1rem;
+}
+#club-main-btn span{
+  font-size: 1.2rem;
+}
+</style>
---
+++ frontend/src/views/club/components/ManageClubButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-dialog
+    width="70rem"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        width="40%"
+        rounded
+        outlined
+        v-bind="attrs"
+        v-on="on"
+        style="background-color: white;"
+      >
+        <span>클럽관리</span>
+      </v-btn>
+    </template>
+    <DialogManageClub
+      :ClubInfo="ClubInfo"
+    />
+  </v-dialog>
+</template>
+<script>
+import DialogManageClub from '@/views/club/components/DialogManageClub.vue';
+
+export default {
+  name: 'ManageClubButton',
+  components: {
+    DialogManageClub,
+  },
+  props: {
+    ClubInfo: Object,
+  },
+};
+</script>
+<style scoped>
+#manage-club-form {
+  padding: 2rem;
+  border-radius: 1rem;
+}
+#manage-club-button {
+  color: black;
+  background-color: white;
+}
+</style>
