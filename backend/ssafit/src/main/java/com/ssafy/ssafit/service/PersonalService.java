02e69db8f294f3937f27e71490b87e33914b1120
김중재 <rlawndwo1305@naver.com>
2022-02-10 16:11:40 +0000

    Deleted backend/ssafit/src/main/java/com/ssafy/ssafit/service/ClubService.java, backend/ssafit/src/main/java/com/ssafy/ssafit/service/ClubServiceImpl.java, backend/ssafit/src/main/java/com/ssafy/ssafit/service/PersonalService.java, backend/ssafit/src/main/java/com/ssafy/ssafit/service/PersonalServiceImpl.java, backend/ssafit/src/main/java/com/ssafy/ssafit/service/UserService.java, backend/ssafit/src/main/java/com/ssafy/ssafit/service/UserServiceImpl.java files

--- backend/ssafit/src/main/java/com/ssafy/ssafit/service/PersonalService.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.ssafit.service;
-
-public interface PersonalService {
-}
--- backend/ssafit/src/main/java/com/ssafy/ssafit/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.ssafit.service;
-
-import com.ssafy.ssafit.db.entity.Club;
-
-public interface UserService {
-
-}
