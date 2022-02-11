514c5f7ece30de41f27abaa2b5da160fcbfbe898
mina <hoy8789@naver.com>
2022-02-11 17:48:20 +0900

    fix | teachable machine camera

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,5 @@
-// const path = require('path');
-
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  // chainWebpack: (config) => {
-  //   config.resolve.alias
-  //     .set('@@', path.resolve(__dirname, 'public/'));
-  // },
 };
