18764c141bc575f546280cf37b0939b0bcfef6b7
mina <hoy8789@naver.com>
2022-02-16 03:34:41 +0900

    feature|마이페이지

--- frontend/src/store/modules/accountsStore.js
+++ frontend/src/store/modules/accountsStore.js
@@ -1,17 +1,68 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 const accountsStore = {
   namespace: true,
   state: {
-    SERVER: 'http://localhost:8081',
+    exDays: [],
+    exTimes: 0,
+    exCals: 0,
   },
   getters: {
 
   },
   mutations: {
-
+    GET_EX_DAYS(state, arr) {
+      state.exDays = arr;
+    },
+    GET_EX_TIMES(state, val) {
+      state.exTimes = val;
+    },
+    GET_EX_CALS(state, val) {
+      state.exCals = val;
+    },
   },
   actions: {
+    // TODO: 로그인 시점에 실행시키기
+    getExDays({ commit }) {
+      // TODO: change userId
+      const userId = 1;
+      function ddmmyyyy(dateTime) {
+        const result1 = dateTime.split('-');
+        const result2 = result1[2].split('T');
+        const date = `${Number(result2[0])}/${Number(result1[1])}/${result1[0]}`;
+        return date;
+      }
+      axios.get(`http://localhost:8081/mypage/${userId}`)
+        .then((res) => {
+          // 데이터 중 날짜만 필터링
+          const result = res.data.MyExerciseLog;
+          const exDate = result.map((v) => ddmmyyyy(String(v.exTime)));
+          commit('GET_EX_DAYS', Array.from(new Set(exDate)));
+        });
+    },
+    getTime({ commit }) {
+      // TODO: change userId
+      const userId = 1;
+      axios.get(`http://localhost:8081/mypage/${userId}`)
+        .then((res) => {
+          const result = res.data.MyExerciseLog;
+          const sec = result.map((v) => v.exDuration).reduce((sum, val) => sum + val, 0);
+          const hour = parseInt(sec / 3600, 10);
+          const min = parseInt((sec % 3600) / 60, 10);
+          const exTime = `${hour}h ${min}m`;
+          commit('GET_EX_TIMES', exTime);
+        });
+    },
+    getCal({ commit }) {
+      // TODO: change userId
+      const userId = 1;
+      axios.get(`http://localhost:8081/mypage/${userId}`)
+        .then((res) => {
+          const result = res.data.MyExerciseLog;
+          const exCal = result.map((v) => v.exCal).reduce((sum, val) => sum + val, 0);
+          commit('GET_EX_CALS', exCal);
+        });
+    },
   },
 };
 
